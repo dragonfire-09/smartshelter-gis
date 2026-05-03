cf23b8726948a5b4658b00ba68f83130a9c4cb9c
Mehmet Cam <73923157+dragonfire-09@users.noreply.github.com>
2026-05-03 20:42:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="SmartShelter GIS",
+    page_icon="🐾",
+    layout="wide"
+)
+
+st.title("🐾 SmartShelter GIS")
+st.write("Deploy testi başarılı. Final uygulama kodu bir sonraki adımda eklenecek.")
